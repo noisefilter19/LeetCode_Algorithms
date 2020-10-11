38bf642aef0660b247c131491fb47183af03a36a
SushanthReddyManda <m.sushanth@iitg.ac.in>
2020-10-11 14:26:18 +0530

    Update Min operations to make Array Equal

--- Python/Minimum Operations to Make Array Equal.py
+++ Python/Minimum Operations to Make Array Equal.py
@@ -8,16 +8,16 @@
 
 ## Examples
 
-Example 1:
+# Example 1:
 
-```
-Input: n = 3
-Output: 2
-Explanation: arr = [1, 3, 5]
-First operation choose x = 2 and y = 0, this leads arr to be [2, 3, 4]
-In the second operation choose x = 2 and y = 0 again, thus arr = [3, 3, 3].
 
-```
+# Input: n = 3
+# Output: 2
+# Explanation: arr = [1, 3, 5]
+# First operation choose x = 2 and y = 0, this leads arr to be [2, 3, 4]
+# In the second operation choose x = 2 and y = 0 again, thus arr = [3, 3, 3].
+
+
 
 ## Code
 
