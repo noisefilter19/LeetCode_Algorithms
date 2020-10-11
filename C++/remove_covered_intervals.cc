6e778e1a232b9ef3c71233b2f7e75f174ae81f69
Hitesh C <jainhitesh9998@gmail.com>
2020-10-11 16:38:08 +0530

    Create remove_covered_intervals.cc

---
+++ C++/remove_covered_intervals.cc
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin(), intervals.end(), myFn);
+        int end, prev_end = 0;
+        int count = 0;
+        for(auto curr: intervals) {
+            end = curr[1];
+            if(prev_end < end){
+                ++count;
+                prev_end = end;
+            }
+        }
+        return count;
+    }
+    static bool myFn(vector<int> &a, vector<int> &b){
+            return a[0]==b[0] ? a[1]>=b[1]: a[0] < b[0];
+    }
+};
