d2003b7cb715a318d9de423fc0edad8fb50b1df9
Tushar Sethi <sethitushar2000@gmail.com>
2020-10-12 00:54:47 +0530

    Solution to problem no. 1299 and 1436 of LeetCode.

---
+++ Python/Destination_City.py
@@ -0,0 +1,12 @@
+# Link to the problem - https://leetcode.com/problems/destination-city/
+
+class Solution(object):
+    def destCity(self, paths):
+        origin, destination = [], []
+        for p in paths:
+            origin.append(p[0])
+            destination.append(p[1])
+
+        for d in destination:
+            if d not in origin :
+                return d 
