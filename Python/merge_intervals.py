06d96a97e0ddba0227fac479d2e870deff869ad6
Raghav Aggarwal <38751567+AggRag@users.noreply.github.com>
2020-10-11 22:44:19 +0530

    Update merge_intervals.py

--- Python/merge_intervals.py
+++ Python/merge_intervals.py
@@ -1,6 +1,4 @@
-Python 3.7.7 (tags/v3.7.7:d7c567b08f, Mar 10 2020, 10:41:24) [MSC v.1900 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if len(intervals)==0:
             return []
         meetings = sorted(intervals)
