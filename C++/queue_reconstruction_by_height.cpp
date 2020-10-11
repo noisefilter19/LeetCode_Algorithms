23d4fd0f668fe4fca066dfcc550f1ccab740f6b2
milanbavishi <milan.bavishi@unacademy.com>
2020-10-11 11:32:21 +0530

    add comments to explain solution

--- C++/queue_reconstruction_by_height.cpp
+++ C++/queue_reconstruction_by_height.cpp
@@ -7,7 +7,6 @@ public:
         vector<vector<int>> ret(people.size());
         sort(people.begin(),people.end(),comp);
         for(vector<int> v: people) {
-            cout<<v[0]<<" "<<v[1]<<endl;
             int x = v[1];
             for(int i=0;i<ret.size();i++) {
                 if(ret[i].size()==0 || ret[i][0]==v[0]) {
@@ -22,3 +21,14 @@ public:
         return ret;
     }
 };
+
+/*
+ * consider the pair p<i,j> where i=height and j=no. of people with height >= height of p
+ * first we place all the shortest height pairs to position j because they will anyway have someone with height >= their height
+ * Then we place second shortest height and so on
+ * suppose the pair p<i,j> is being processed
+ * we need to keep p at jth empty position so x=j
+ * we decrement x whenever we find an empty position or someone with same height because at empty postion there will be someone 
+ * with height >= height of p
+ * Need not to check for >= condition as we have sorted the array before so no one in ret array would be having height > height * of p 
+ */
