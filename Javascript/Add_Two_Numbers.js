2b4eba4ab1b232318f23855867c13feccf868991
Mario (BU) Rey II <mreybu@optimy.com>
2020-10-14 23:50:48 +0800

    Improve condition.

--- Javascript/Add_Two_Numbers.js
+++ Javascript/Add_Two_Numbers.js
@@ -6,7 +6,11 @@
 const addTwoNumbers = (list1, list2, carry = 0) => {
     const sum = (list1?.val ?? 0) + (list2?.val ?? 0) + carry;
     carry = sum > 9 ? 1 : 0;
-    if (list1?.next === null && list2?.next === null) {
+    if (
+        (list1 === undefined || list1 === null || list1.next === null) &&
+        (list2 === undefined || list2 === null || list2.next === null) &&
+        carry == 0
+    ) {
         return new ListNode(sum % 10, null);
     }
     return new ListNode(sum % 10, addTwoNumbers(list1?.next, list2?.next, carry));
