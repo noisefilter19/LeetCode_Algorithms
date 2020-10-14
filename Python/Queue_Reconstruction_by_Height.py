dc74f2e461967ec44f8b22bacc7a01c007b36c93
frank731 <38197177+frank731@users.noreply.github.com>
2020-10-13 18:51:22 -0700

    Removed unnecessary print statement

--- Python/Queue_Reconstruction_by_Height.py
+++ Python/Queue_Reconstruction_by_Height.py
@@ -3,7 +3,6 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
         people.sort(key=lambda person: (-person[0], person[1]))
-        print(people)
         sorted_queue = []
         for person in people:
             sorted_queue.insert(person[1], person)
