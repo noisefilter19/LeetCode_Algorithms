3b1feca4b7d24d34f71cc83b743a81e34a9d1e5d
Francesco Urbani <francescourbanidue@gmail.com>
2020-10-15 10:46:22 +0200

    added leetcode 71 (Simplify path)

---
+++ C++/Simplify_path.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/simplify-path/
+
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string res, tmp;
+        vector<string> stk;
+        stringstream ss(path);
+     
+        while (getline(ss,tmp,'/')) {
+            if (tmp == "" or tmp == ".") continue;
+            if (tmp == ".." and !stk.empty()) stk.pop_back();
+            else if (tmp != "..") stk.push_back(tmp);
+        }
+     
+        for (auto str : stk) res += "/"+str;
+        return res.empty() ? "/" : res;
+    }      
+};
