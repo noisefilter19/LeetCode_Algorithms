6ebba302d8ffe5bd2f75c8a162a2ea977fc84eee
omkarraut333 <53116480+omkarraut333@users.noreply.github.com>
2020-10-15 17:11:00 +0530

    Update Jump_Game.py

--- Python/Jump_Game.py
+++ Python/Jump_Game.py
@@ -1,10 +1,13 @@
+# A class with name 'Solution'
 class Solution:
-    def canJump(self, nums: List[int]) -> bool:
+    def canJump(self, nums: List[int]) -> bool:         # function in a class
         lastpos=len(nums)-1
-        for i in range(len(nums)-1,-1,-1):
-            if i+nums[i]>=lastpos:
+        for i in range(len(nums)-1,-1,-1):              # for loop
+            if i+nums[i]>=lastpos:                      # if Statement
                 lastpos=i
         if lastpos==0:
-            return True
-        else:
+            return True                                 # return statement
+        else:                                           # else statement
             return False
+
+        # End of the code
