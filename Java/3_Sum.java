3822c15ce0aaac3c305182252ebeaa0c856046c2
jdk <44255908+Jambuvant@users.noreply.github.com>
2020-10-15 15:54:33 +0530

    added problem description

--- Java/3_Sum.java
+++ Java/3_Sum.java
@@ -1,29 +1,39 @@
 //Problem Link: https://leetcode.com/problems/3sum/
+// Problem Description:
+/* Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? 
+ * Find all unique triplets in the array which gives the sum of zero.
+ * Notice that the solution set must not contain duplicate triplets.
+ 
+ * Input: nums = [-1,0,1,2,-1,-4]
+ * Output: [[-1,-1,2],[-1,0,1]]
+ */
 
 //Solution
 
 class Solution {
+    // function threeSum takes an array of integers as a argument
+    // returns a list of lists of integers
     public List<List<Integer>> threeSum(int[] nums) {
-        int i, j;
+        int i = 0, j = 0;
         Arrays.sort(nums);
     
-        List<List<Integer>> res = new ArrayList() ;
+        List<List<Integer>> res = new ArrayList();
         
-        for(int k = 0 ; k < nums.length-1 ; k++){
-            i =k+1;
+        for(int k = 0; k < nums.length-1; k++) {
+            i = k + 1;
             j = nums.length-1;
             int ele = nums[k];
             
-            if(  k > 0 && nums[k] == nums[k-1])  continue;
+            if (k > 0 && nums[k] == nums[k-1])  continue;
             
-            while( i < j){
+            while ( i < j){
                 
-                if(  i-1 > k &&  nums[i] == nums[i-1] ){
+                if (i-1 > k &&  nums[i] == nums[i-1]) {
                     i++;
                     continue;
                 }
                 
-                if( -ele == nums[i] + nums[j] ){
+                if ( -ele == nums[i] + nums[j]) {
                     List<Integer> list = new ArrayList();
                     list.add(ele);
                     list.add(nums[i]);
@@ -33,10 +43,8 @@ class Solution {
                     i++;
                     j--;
                 }
-                else if( -ele > nums[i] + nums[j] )
-                    i++;
-                else 
-                    j--;                                   
+                else if (-ele > nums[i] + nums[j])  i++;
+                else  j--;                                   
             }
         } 
         return res;
