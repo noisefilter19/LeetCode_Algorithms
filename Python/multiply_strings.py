66363e54c59465b2f06a9a610048c6b57198b750
d3xt3r0 <prathapss87@gmail.com>
2020-10-16 11:49:22 +0530

    Add multiply_strings solution in python

---
+++ Python/multiply_strings.py
@@ -0,0 +1,5 @@
+# LeetCode Problem link - https://leetcode.com/problems/multiply-strings/submissions/
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        return ""+str(int(num1)*int(num2))
