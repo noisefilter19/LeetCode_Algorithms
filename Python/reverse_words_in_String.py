6d0987117f4522bf0cc367ede0e1fadf58a2f00b
Aathira <aathirasatheeshnair@gmail.com>
2020-10-17 11:30:01 +0530

    Linked PR with issue

--- Python/reverse_words_in_String.py
+++ Python/reverse_words_in_String.py
@@ -5,7 +5,6 @@ Return a string of the words in reverse order concatenated by a single space.
 Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
 problem link : https://leetcode.com/problems/reverse-words-in-a-string/
 '''
-
 class Solution:
     def reverseWords(self, s: str) -> str:
         
