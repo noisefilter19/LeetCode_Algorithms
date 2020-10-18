0da2f029f6479be870e98b8d8ac8cc2c42f50c29
Dhiraj Jaiswal <dhiraj.jaiswal96@gmail.com>
2020-10-18 15:09:11 +0530

    Delete group_Anagram.py

--- Python/group_Anagram.py
+++
@@ -1,7 +0,0 @@
-def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-	dic = collections.defaultdict(list)
-	for st in strs:
-		s = ''.join(sorted(st))
-		dic[s].append(st)
-
-	return dic.values()
