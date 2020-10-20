33c2cb70da72d654133603226dbec04dbc07b743
Christian Clauss <cclauss@me.com>
2020-10-20 12:56:23 +0200

    xrange() was removed from Python on 1/1/2020

--- Python/Generate Paranthesis.py
+++ Python/Generate Paranthesis.py
@@ -2,7 +2,7 @@ class Solution(object):
     def generateParenthesis(self, N):
         if N == 0: return ['']
         ans = []
-        for c in xrange(N):
+        for c in range(N):
             for left in self.generateParenthesis(c):
                 for right in self.generateParenthesis(N-1-c):
                     ans.append('({}){}'.format(left, right))
