5d3a3539042229de77f6329abd5014e96f5e8bf5
Christian Clauss <cclauss@me.com>
2020-10-20 13:04:10 +0200

    from typing import List

--- Python/449_Serialize_and_Deserialize_BST.py
+++ Python/449_Serialize_and_Deserialize_BST.py
@@ -1,9 +1,14 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+from typing import List
+
+
+class TreeNode:
+    """
+    Binary tree node
+    """
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Codec:
 
