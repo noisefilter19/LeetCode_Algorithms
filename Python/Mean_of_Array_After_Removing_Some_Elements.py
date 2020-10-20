d1cda1d1068f31da08e5eba222a89fc486f2ff5b
Aryan Sarkar <46970814+aryansarkar13@users.noreply.github.com>
2020-10-20 21:35:59 +0530

    #Issue 625 QNo 1619 | Language Python

---
+++ Python/Mean_of_Array_After_Removing_Some_Elements.py
@@ -0,0 +1,11 @@
+class Solution:
+    def trimMean(self, arr: List[int]) -> float:
+        n = len(arr)
+        arr = sorted(arr)
+        starti = int(n * 0.05)
+        new_arr = arr[starti:n-starti]
+        new_len = len(new_arr)
+        if new_len == 0:
+            return 0
+        ans = sum(new_arr)/new_len
+        return ans
