506530dd3333fd8c358f0fb04cdde4eaad72948e
javmarina <javmarina@uma.es>
2020-10-24 21:11:09 +0200

    Fixed bad text copy

--- Python/636_Exclusive_Time_Of_Functions.py
+++ Python/636_Exclusive_Time_Of_Functions.py
@@ -1,29 +1,30 @@
 # https://leetcode.com/problems/exclusive-time-of-functions/
-class Solution(object)
-    def exclusiveTime(self, n, logs)
-        
-        type n int
-        type logs List[str]
-        rtype List[int]
-        
+
+class Solution(object):
+    def exclusiveTime(self, n, logs):
+        """
+        :type n: int
+        :type logs: List[str]
+        :rtype: List[int]
+        """
         
         time = [0 for i in range(n)]
         stack = []
         last_timestamp = 0
-        for log in logs
-            parts = log.split()
+        for log in logs:
+            parts = log.split(":")
             function_id = int(parts[0])
-            started = parts[1] == start
+            started = parts[1] == "start"
             timestamp = int(parts[2])
-            if not started
+            if not started:
                 timestamp += 1
             
-            if stack
+            if stack:
                 active_id = stack[-1]
                 time[active_id] = time[active_id] + (timestamp-last_timestamp)
-            if started
+            if started:
                 stack.append(function_id)
-            else
+            else:
                 stack.pop()
             
             last_timestamp = timestamp
