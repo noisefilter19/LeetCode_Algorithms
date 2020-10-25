145b6bc70ca0ccae35d20c29a1a141be2c551edc
Alekhya77 <54280241+Alekhya77@users.noreply.github.com>
2020-10-25 12:41:31 +0530

    Update 50|Pow(x, n).py

--- Python/50|Pow(x, n).py
+++ Python/50|Pow(x, n).py
@@ -1,7 +1,5 @@
 # Problem Link : https://leetcode.com/problems/powx-n/
 
-import math
-
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return pow(x, n)
+        return x ** n
