4a6a9b5dd32bb35a718f311d60b5b3f802dc307c
Ingrid Rodríguez <ingirodriguez.91@gmail.com>
2020-10-28 14:43:28 -0500

    3sumclosest typescript fixed

--- Typescript/16_3Sum Closest.ts
+++ Typescript/16_3Sum Closest.ts
@@ -2,16 +2,25 @@
  * https://leetcode.com/problems/3sum-closest/
  */
 
-function threeSumClosest(nums: number[], target: number): number {
+function threeSumClosest(a: number[], target: number): number {
     let count = 0;
     let sum =0;
-    let result = 0;
-    for (count; count<3; count++){
-        result = nums.reduce((a, b) => {
-            return Math.abs(b - target) < Math.abs(a - target) ? b : a;
-        });
-        nums.splice(nums.indexOf(result), 1); 
-        sum=sum+result;
-    }
-    return sum;
+    var result = [];
+    let closest = 0;
+
+    for (var i = 0; i < a.length-2; i++) {
+      for (var j = i+1; j< a.length-1;j++) {
+        for (var k = j+1; k < a.length;k++) {
+            let sum = a[i]+a[j]+a[k] 
+             result.push(sum);
+        }
+      }
+    }    
+    
+    closest = result.reduce((a, b) => {
+        return Math.abs(b - target) < Math.abs(a - target) ? b : a;
+    });
+
+    console.log(closest);
+    return closest;
 };
