9f435c280e51ba0766d8a3e2c9a8ecdf94b8f0bb
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-10-01 14:28:24 +0530

    Fixed Issue#897

---
+++ C++/Permutations_II.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        do{
+            ans.push_back(nums);
+        }while(next_permutation(nums.begin(),nums.end()));
+        sort(ans.begin(),ans.end());
+        ans.erase(unique(ans.begin(),ans.end()),ans.end());
+        return ans;
+    }
+};
