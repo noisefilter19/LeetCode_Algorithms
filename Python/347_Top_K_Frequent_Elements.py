be1ac4aa9aa0c2a7fcb79d43089c33f6f9cc3e0f
Sakshi Vattikuti <51375116+sakshi2912@users.noreply.github.com>
2021-10-04 19:01:41 +0530

    Update 347_Top_K_Frequent_Elements.py

--- Python/347_Top_K_Frequent_Elements.py
+++ Python/347_Top_K_Frequent_Elements.py
@@ -1,3 +1,14 @@
+'''
+Using python's library: collections
+# from collections import Counter
+
+Use the counter object to store all frequencies of the list.
+Usimg the most_common method, which takes an argument N, signifying the number of most common elements to be returned.
+
+The returned list consists of tuples (num, freq).
+Return the first element of each tuple as the final answer.
+
+'''
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
